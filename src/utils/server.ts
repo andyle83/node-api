485282edf5a0d1fa43d0e97c7b357a26e4cecca9
WGTECH\anh.le <anh.le@beteasy.com.au>
2021-08-05 09:01:20 +1000

    feat: adding new controller method with auth

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,51 @@
 import express from "express";
-import {Express} from "express-serve-static-core";
+import * as OpenApiValidator from 'express-openapi-validator';
+import { Express } from 'express-serve-static-core';
+import { connector, summarise } from 'swagger-routes-express';
+import YAML from 'yamljs';
+
+import * as api from '../api/controllers';
 
 export async function createServer(): Promise<Express> {
+  const yamlSpecFile = './config/openapi.yml';
+  const apiDefinition = YAML.load(yamlSpecFile);
+  const apiSummary = summarise(apiDefinition);
+  console.info(apiSummary);
+
+  // Server init
   const server = express();
-  server.get("/", (req, res) => {
-    res.send("Hello World.");
+
+  // Setup API validator
+  const validatorOptions = {
+    apiSpec: yamlSpecFile,
+    validateRequests: true,
+    validateResponses: true
+  };
+
+  server.use(OpenApiValidator.middleware(validatorOptions));
+
+  // Error customization, if request is invalid
+  server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(err.status).json({
+      error: {
+        type: 'request_validation',
+        message: err.message,
+        errors: err.errors
+      }
+    });
   });
+
+  const connect = connector(api, apiDefinition, {
+    onCreateRoute: (method: string, descriptor: any[]) => {
+      descriptor.shift();
+      console.log(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`)
+    },
+    security: {
+      bearerAuth: api.auth
+    }
+  });
+
+  connect(server);
+
   return server;
 }
