03cf905e298e5303c93e199f02c66855b26571ae
WGTECH\anh.le <anh.le@beteasy.com.au>
2021-08-06 12:29:05 +1000

    feat: adding mongodb configure

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,15 +14,25 @@ const env = dotenvExtended.load({
 const parsedEnv = dotenvParseVariables(env);
  
 interface Config {
-  morganLogger: boolean
-  morganBodyLogger: boolean
-  exmplDevLogger: boolean
+  morganLogger: boolean,
+  morganBodyLogger: boolean,
+  exmplDevLogger: boolean,
+  mongo: {
+    url: string,
+    useCreateIndex: boolean,
+    autoIndex: boolean
+  },
 }
 
 const config: Config = {
   morganLogger: parsedEnv.MORGAN_LOGGER as boolean,
   morganBodyLogger: parsedEnv.MORGAN_BODY_LOGGER as boolean,
-  exmplDevLogger: parsedEnv.EXMPL_DEV_LOGGER as boolean
+  exmplDevLogger: parsedEnv.EXMPL_DEV_LOGGER as boolean,
+  mongo: {
+    url: parsedEnv.MONGO_URL as string,
+    useCreateIndex: parsedEnv.MONGO_CREATE_INDEX as boolean,
+    autoIndex: parsedEnv.MONGO_AUTO_INDEX as boolean
+  },
 };
 
 export default config;
